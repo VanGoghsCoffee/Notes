5c443bc48935a33e5034cc1033ef84ee473d75d5
VanGoghsCoffee <mail@davidrochholz.de>
2013-10-09 17:54:19 +0200

    Clean commit

--- src/php/Settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-#-----------------------------------------------
-# HTML Title and timezone
-#-----------------------------------------------
-define('HTML_TITLE', 'Notes');
-date_default_timezone_set('Europe/Berlin');
-
-?>
